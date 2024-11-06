8dbdeb955f89019c616af1cc181da2e781a95b00
Andre Cadete <andrecadete70@gmail.com>
2024-11-06 15:23:47 +0100

    little changes

--- level-3_2.ts
+++ level-3_2.ts
@@ -17,11 +17,13 @@ const myAgeIn2007 = differenceInYears(new Date(2007, 3, 7), myBirthday);
 console.log(`Ich bin ${myAge} Jahre alt`);
 console.log(`In 2007 I was ${myAgeIn2007} years old`);
 
-console.log(format(myBirthday, 'dd.MM.yyyy hh:mm:ss'));
-console.log(format(myBirthday, 'dd.MM.yyyy hh:mm'));
+console.log(format(myBirthday, 'dd.MM.yyyy kk:mm:ss'));
+console.log(format(myBirthday, 'dd.MM.yyyy kk:mm'));
 console.log(format(myBirthday, 'dd.MM.yy'));
 console.log(format(myBirthday, 'dd. MMMM yyyy'));
-console.log(format(myBirthday, 'hh:mm'));
+console.log(format(myBirthday, 'kk:mm'));
 console.log(myBirthday.getDay() === 4 ? 'Donnerstag' : null);
-console.log(myBirthday.getMonth() === 7 ? 'August' : null);
+console.log(format(myBirthday, 'EEEE'));
+console.log(format(myBirthday, 'LLLL'));
 console.log(`${myBirthday.getDate()} des ${myBirthday.getMonth() === 7 ? 'August' : null} im Jahre des Herrn ${myBirthday.getFullYear()}`);
+console.log(format(myBirthday, "dd 'of' MMMM 'in year' yyyy"));
